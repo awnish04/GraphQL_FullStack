8cda86c09f15d82946b59c8fdbedaf560430b8dd
AWNISH MEHTA <awnishmehta69@gmail.com>
2025-11-11 21:26:42 +0545

    Updated Dashboard Layouts

--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
+import { ChartBarInteractive } from "@/components/ui/AreaChart/chart-bar-interactive";
+import { ChartAreaInteractive } from "@/components/ui/AreaChart/salesdetailschart";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Users, Box, ChartLine, History } from "lucide-react";
-// import { AnalyticsChart } from "@/components/ui/analytics";
-// import { PieChart } from "@/components/ui/pie-chart";
-// import { BarChartComponent } from "@/components/ui/barchart";
-// import { SalesDetailsChart } from "@/components/ui/salesdetailschart";
-// import { useActivePage } from "hooks/use-active-page";
 export default function DashboardPage() {
   // const pageTitle = useActivePage();
   return (
@@ -15,7 +12,7 @@ export default function DashboardPage() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Users</CardTitle>
-            <Users className="h-6 w-6 " />
+            <Users className="h-10 w-10 text-primary" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">24</div>
@@ -23,8 +20,10 @@ export default function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Order</CardTitle>
-            <Box className="h-6 w-6 " />
+            <CardTitle className="text-sm font-medium">
+              Total Projects
+            </CardTitle>
+            <Box className="h-10 w-10 text-primary" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">12</div>
@@ -32,8 +31,8 @@ export default function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Sales</CardTitle>
-            <ChartLine className="h-6 w-6 " />
+            <CardTitle className="text-sm font-medium">Total Skills</CardTitle>
+            <ChartLine className="h-10 w-10 text-primary" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">8</div>
@@ -41,21 +40,20 @@ export default function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Pending</CardTitle>
-            <History className="h-6 w-6 " />
+            <CardTitle className="text-sm font-medium">
+              Pending Projects
+            </CardTitle>
+            <History className="h-10 w-10 text-primary" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">6</div>
           </CardContent>
         </Card>
       </div>
-      {/* <SalesDetailsChart />
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
-        <AnalyticsChart />
-
-        <PieChart />
-      </div>
-      <BarChartComponent /> */}
+      {/* <SalesDetailsChart /> */}
+      <ChartAreaInteractive />
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2"></div>
+      <ChartBarInteractive />
     </div>
   );
 }
